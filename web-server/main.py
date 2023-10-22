0f032578622d8347a9e3780e00d4daddfa671d31
gaboiv <gabrielcaraballo1907@gmail.com>
2023-10-21 23:09:36 -0500

    add: docker for app, web-server projects

--- web-server/main.py
+++ web-server/main.py
@@ -1,4 +1,20 @@
 import store
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+@app.get('/')
+def get_list():
+    return [1,2,3]
+
+@app.get('/contact', response_class = HTMLResponse)
+def get_contact():
+    # return { 'name': 'GaboStore' }
+    return """
+        <h1>Soy una página</h1>
+        <p>Esto es un párrafo que desmuestra que es un html genuino</p>
+    """
 
 def run():
     store.get_categories()
